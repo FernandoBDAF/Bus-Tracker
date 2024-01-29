b99bcb45abb3b2ea41e873dfa325ed6aeebfd363
Fernando Barroso <fernandobarroso@suaquadra.com.br>
2024-01-28 20:04:41 -0700

    final adjustments

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
-#Real Time Bus Tracker
+# Real Time Bus Tracker
 
-I added markers after 15 seconds to track the route of each bus. For that matter, I choose a color for the marker according to the ID of the bus and I also add a tag to track the insertion time. On top of that, I save pointers to all markers to further improvements such as hiding/showing in the map.
+<img src="bus-tracker.png" width="33%">
 
-I did these changes because I would like to keep track of the route instead of only the instant position, but having the instant location of all buses or the tracks of the routes is another improvement I will make in the next version.
+## Description
 
-[x] show time runing
-[ ] button to choose real time position only or follow route
-[x] show list of buses by id
-[x] function to hide/show markers of a specific bus
-[ ] show detailed informations of each bus in the list
-[ ] improve desing
+This project integrates Mapbox and the MBTA API to showcase real-time bus locations on an interactive map. The buses' movements are actively monitored and listed in a side panel. Every 15 seconds, markers are strategically placed to trace the route of each bus. Each marker is color-coded based on the bus ID, and timestamps are included for insertion tracking. Additionally, users have the flexibility to toggle visibility for all data on the map or selectively display specific buses by their IDs using the panel.
+
+## How to Run
+
+1. Clone the repository to your local machine.
+2. Open the `index.html` file in your preferred web browser.
+3. Interact with the map using the provided buttons:
+   - Hide/Show Markers
+   - List All Buses
+   - Check Live Updates
+4. Click the "Live" button to initiate real-time bus tracking. You can pause and restart the tracking as needed while retaining the displayed points.
+
+## Roadmap of Future Improvements
+
+- Enhance user control with the ability to hide/show specific buses on the map.
+- Provide a user-friendly panel for selecting and displaying buses based on their unique IDs.
+- Implement real-time updates for instant bus locations on the map.
+- Enable detailed route tracking to offer a more comprehensive data visualization experience.
--- mapanimation.js
+++ mapanimation.js
@@ -1,63 +1,120 @@
 async function getBusLocations() {
-    const url =
-      "https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip";
-    const response = await fetch(url);
-    const json = await response.json();
-    return json.data;
+  const url = "https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip";
+  const response = await fetch(url);
+  const json = await response.json();
+  return json.data;
+}
+
+async function run() {
+  const buses = await getBusLocations();
+  return buses;
+}
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZmJkYWYiLCJhIjoiY2xycGgyaGtlMDRvZDJqbjdiaTJ4N3lodSJ9._MyPu5Nu9mA_TvnKM4fUjg";
+
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-71.104081, 42.365554],
+  zoom: 12,
+});
+
+function hideMarker(id) {
+  if (id in markers) {
+    console.log(id);
+    markers[id].forEach((marker) => {
+      marker[0].remove();
+      marker[1].remove();
+    });
   }
+}
 
-  async function run() {
-    const buses = await getBusLocations();
-    return buses;
+function showMarker(id) {
+  if (id in markers) {
+    console.log(id);
+    markers[id].forEach((marker) => {
+      marker[0].addTo(map);
+      marker[1].addTo(map);
+    });
   }
+}
 
-  mapboxgl.accessToken =
-    "pk.eyJ1IjoiZmJkYWYiLCJhIjoiY2xycGgyaGtlMDRvZDJqbjdiaTJ4N3lodSJ9._MyPu5Nu9mA_TvnKM4fUjg";
-
-  var map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v11",
-    center: [-71.104081, 42.365554],
-    zoom: 12,
-  });
-
-  function hideMarker(id) {
-    if (id in markers) {
-      console.log(id);
-      markers[id].forEach((marker) => {
-        marker[0].remove();
-        marker[1].remove();
-      });
-    }
+function hideAll() {
+  for (const id in markers) {
+    hideMarker(id);
   }
+}
 
-  function showMarker(id) {
-    if (id in markers) {
-      console.log(id);
-      markers[id].forEach((marker) => {
-        marker[0].addTo(map);
-        marker[1].addTo(map);
-      });
-    }
+function showAll() {
+  for (const id in markers) {
+    showMarker(id);
   }
+}
 
-  function hideAll() {
-    for (const id in markers) {
-      hideMarker(id);
-    }
-  }
-
-  function showAll() {
-    for (const id in markers) {
-      showMarker(id);
-    }
-  }
-
-var color = ["#FF5733", "#3399FF", "#33FF57", "#FF3399", "#FF33FF", "#FFFF33", "#33FFFF", "#FF5733", "#3399FF", "#33FF57", "#FF3399", "#FF33FF", "#FFFF33", "#33FFFF", "#FF5733", "#3399FF", "#33FF57", "#FF3399", "#FF33FF", "#FFFF33", "#33FFFF", "#FF5733", "#3399FF", "#33FF57", "#FF3399", "#FF33FF", "#FFFF33", "#33FFFF", "#FF5733", "#3399FF", "#33FF57", "#FF3399", "#FF33FF", "#FFFF33", "#33FFFF", "#FF5733", "#3399FF", "#33FF57", "#FF3399", "#FF33FF", "#FFFF33", "#33FFFF", "#FF5733", "#3399FF", "#33FF57", "#FF3399", "#FF33FF", "#FFFF33", "#33FFFF", "#FF5733", "#3399FF", "#33FF57", "#FF3399", "#FF33FF", "#FFFF33", "#33FFFF"];
+var color = [
+  "#FF5733",
+  "#3399FF",
+  "#33FF57",
+  "#FF3399",
+  "#FF33FF",
+  "#FFFF33",
+  "#33FFFF",
+  "#FF5733",
+  "#3399FF",
+  "#33FF57",
+  "#FF3399",
+  "#FF33FF",
+  "#FFFF33",
+  "#33FFFF",
+  "#FF5733",
+  "#3399FF",
+  "#33FF57",
+  "#FF3399",
+  "#FF33FF",
+  "#FFFF33",
+  "#33FFFF",
+  "#FF5733",
+  "#3399FF",
+  "#33FF57",
+  "#FF3399",
+  "#FF33FF",
+  "#FFFF33",
+  "#33FFFF",
+  "#FF5733",
+  "#3399FF",
+  "#33FF57",
+  "#FF3399",
+  "#FF33FF",
+  "#FFFF33",
+  "#33FFFF",
+  "#FF5733",
+  "#3399FF",
+  "#33FF57",
+  "#FF3399",
+  "#FF33FF",
+  "#FFFF33",
+  "#33FFFF",
+  "#FF5733",
+  "#3399FF",
+  "#33FF57",
+  "#FF3399",
+  "#FF33FF",
+  "#FFFF33",
+  "#33FFFF",
+  "#FF5733",
+  "#3399FF",
+  "#33FF57",
+  "#FF3399",
+  "#FF33FF",
+  "#FFFF33",
+  "#33FFFF",
+];
 var c = 0;
 var busColor = {};
 var j = 0;
-var markers = {}
+var markers = {};
+var running = null;
 
 async function setPoints() {
   const buses = await run();
@@ -106,112 +163,114 @@ function listAddMarker(color, id) {
 
   let child = document.createElement("div");
   child.style.borderRadius = "50%";
-    child.style.width = "15px";
-    child.style.height = "15px";
-    child.style.backgroundColor = color;
+  child.style.width = "15px";
+  child.style.height = "15px";
+  child.style.backgroundColor = color;
 
-    let child2 = document.createElement("p");
-    child2.innerHTML = id;
+  let child2 = document.createElement("p");
+  child2.innerHTML = id;
 
-    let child3 = document.createElement("p");
-    child3.innerHTML = markers[id].length;
-    child3.style.backgroundColor = "black";
-    child3.style.color = "white";
-    child3.style.padding = "3px";
+  let child3 = document.createElement("p");
+  child3.innerHTML = markers[id].length;
+  child3.style.backgroundColor = "black";
+  child3.style.color = "white";
+  child3.style.padding = "3px";
 
-    let child4 = document.createElement("button");
-    child4.innerHTML = "H";
-    child4.style.fontSize = "10px";
-    child4.style.backgroundColor = "red";
-    child4.style.color = "white";
-    child4.onclick = function() {
-        hideMarker(id);
-    }
+  let child4 = document.createElement("button");
+  child4.innerHTML = "H";
+  child4.style.fontSize = "10px";
+  child4.style.backgroundColor = "red";
+  child4.style.color = "white";
+  child4.onclick = function () {
+    hideMarker(id);
+  };
 
-    let child5 = document.createElement("button");
-    child5.innerHTML = "S";
-    child5.style.fontSize = "10px";
-    child5.style.backgroundColor = "green";
-    child5.style.color = "white";
-    child5.onclick = function() {
-        showMarker(id);
-    }
+  let child5 = document.createElement("button");
+  child5.innerHTML = "S";
+  child5.style.fontSize = "10px";
+  child5.style.backgroundColor = "green";
+  child5.style.color = "white";
+  child5.onclick = function () {
+    showMarker(id);
+  };
 
-    let child6 = document.createElement("div");
-    // child6.style.display = "flex";
-    // child6.style.flexDirection = "column";
+  let child6 = document.createElement("div");
+  // child6.style.display = "flex";
+  // child6.style.flexDirection = "column";
 
-    child6.appendChild(child4);
-    child6.appendChild(child5);
+  child6.appendChild(child4);
+  child6.appendChild(child5);
 
-    father.appendChild(child);
-    father.appendChild(child2);
-    father.appendChild(child3);
-    father.appendChild(child6);
+  father.appendChild(child);
+  father.appendChild(child2);
+  father.appendChild(child3);
+  father.appendChild(child6);
 
-
-    document.getElementById("buses").appendChild(father);
+  document.getElementById("buses").appendChild(father);
 }
 
 function listRemoveMarkers() {
-    const parentElement = document.getElementById("buses");
-    while (parentElement.firstChild) {
-      parentElement.removeChild(parentElement.firstChild);
-    }
+  const parentElement = document.getElementById("buses");
+  while (parentElement.firstChild) {
+    parentElement.removeChild(parentElement.firstChild);
+  }
 }
 
 function listMarkers() {
-    listRemoveMarkers();
-    for (const id in busColor) {
-        listAddMarker(busColor[id], id);
-    };
+  listRemoveMarkers();
+  for (const id in busColor) {
+    listAddMarker(busColor[id], id);
+  }
 }
 
 function startRunning() {
-    let button = document.getElementById("startButton");
-    if (running) {
-        clearInterval(running);
-        clearInterval(listAll);
-        running = null;
-        button.innerHTML = "Start";
-        button.style.backgroundColor = "green";
-        button.style.color = "white";
-        pauseTimer()
-    } else {
-        running = setInterval(setPoints, 15000);
-        listAll = setInterval(listMarkers, 15000);
-        button.innerHTML = "Stop";
-        button.style.backgroundColor = "red";
-        button.style.color = "white";
-        restartTimer()
-    }
-    
+  let button = document.getElementById("startButton");
+  if (running) {
+    clearInterval(running);
+    clearInterval(listAll);
+    running = null;
+    button.innerHTML = "Start";
+    button.style.backgroundColor = "green";
+    button.style.color = "white";
+    pauseTimer();
+  } else {
+    setPoints();
+    running = setInterval(setPoints, 15000);
+    listAll = setInterval(listMarkers, 15000);
+    button.innerHTML = "Stop";
+    button.style.backgroundColor = "red";
+    button.style.color = "white";
+    restartTimer();
+  }
 }
 
-setPoints();
-var running = setInterval(setPoints, 15000);
-var listAll = setInterval(listMarkers, 15000);
-
+// setPoints();
+// var running = setInterval(setPoints, 15000);
+// var listAll = setInterval(listMarkers, 15000);
 
 // Timer
-const timerContainer = document.getElementById('timer');
+const timerContainer = document.getElementById("timer");
 let timerInterval;
 let totalSeconds = 0;
 
 function updateTimer() {
-    const hours = Math.floor(totalSeconds / 3600).toString().padStart(2, '0');
-    const minutes = Math.floor((totalSeconds % 3600) / 60).toString().padStart(2, '0');
-    const seconds = (totalSeconds % 60).toString().padStart(2, '0');
-    timerContainer.textContent = `${hours}:${minutes}:${seconds}`;
-    totalSeconds++;
+  const hours = Math.floor(totalSeconds / 3600)
+    .toString()
+    .padStart(2, "0");
+  const minutes = Math.floor((totalSeconds % 3600) / 60)
+    .toString()
+    .padStart(2, "0");
+  const seconds = (totalSeconds % 60).toString().padStart(2, "0");
+  timerContainer.textContent = `${hours}:${minutes}:${seconds}`;
+  totalSeconds++;
 }
 
-timerInterval = setInterval(updateTimer, 1000);
+// timerInterval = setInterval(updateTimer, 1000);
 
 function pauseTimer() {
-    clearInterval(timerInterval);
+  clearInterval(timerInterval);
 }
 
 function restartTimer() {
-    timerInterval = setInterval(updateTimer, 1000);
+  timerInterval = setInterval(updateTimer, 1000);
 }
